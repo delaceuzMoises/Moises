78b062ba9e677636261e4b0d54834b585732049d
♡Moises <85186604+delaceuzMoises@users.noreply.github.com>
2021-06-01 12:42:47 -0500

    Update App.php

--- codeingniter/app/Config/App.php
+++ codeingniter/app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'http://localhost:8080/';
+	public $baseURL = 'http://localhost:8080/'; http://cerounodigital.com/btc04/Moises/codeingniter/public/
 
 	/**
 	 * --------------------------------------------------------------------------
